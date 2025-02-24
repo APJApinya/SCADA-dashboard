e01328bebd384df6e3df874e3f755ee46c58e169
APJApinya <apinya.p.jao@gmail.com>
2025-02-24 14:33:28 +1000

    update the button styling

--- components/StartStopButtons.tsx
+++ components/StartStopButtons.tsx
@@ -1,30 +1,44 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_URL = "https://wy9dediidb.execute-api.ap-southeast-2.amazonaws.com"
+const API_URL = "https://wy9dediidb.execute-api.ap-southeast-2.amazonaws.com";
 
 const StartStopButtons = () => {
-    // using state hook to control the button
-    const [isGenerating, setIsGenerating] = useState(false);
+  // using state hook to control the button
+  const [isGenerating, setIsGenerating] = useState(false);
 
-    const startSensorData = async() => {
-        const response = await axios.post(`${API_URL}/start`);
-        console.log(response.data);
-        setIsGenerating(true);
-    };
+  const startSensorData = async () => {
+    const response = await axios.post(`${API_URL}/start`);
+    console.log(response.data);
+    setIsGenerating(true);
+  };
 
-    const stopSensorData = async() => {
-        const response = await axios.post(`${API_URL}/stop`);
-        console.log(response.data);
-        setIsGenerating(false);
-    };
+  const stopSensorData = async () => {
+    const response = await axios.post(`${API_URL}/stop`);
+    console.log(response.data);
+    setIsGenerating(false);
+  };
 
-    return (
-        <div>
-            <button onClick={startSensorData} disabled={isGenerating}>Start the engine</button>
-            <button onClick={stopSensorData} disabled={!isGenerating}>Stop the engine</button>
-        </div>
-    );
+  return (
+    <div className="flex space-x-4 mt-4">
+      {!isGenerating && (
+        <button
+          onClick={startSensorData}
+          className="px-4 py-2 text-white font-semibold rounded-lg shadow-md bg-green-500 hover:bg-green-600 transition"
+        >
+          Start
+        </button>
+      )}
+      {isGenerating && (
+        <button
+          onClick={stopSensorData}
+          className="px-4 py-2 text-white font-semibold rounded-lg shadow-md bg-red-500 hover:bg-red-600 transition"
+        >
+          Stop
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default StartStopButtons;
