cd94e904dc2dc06b480938c7415f7c581ca3fc9c
APJApinya <apinya.p.jao@gmail.com>
2025-02-24 22:06:27 +1000

    add colour when high temp detected

--- app/page.tsx
+++ app/page.tsx
@@ -15,22 +15,27 @@ const Dashboard: React.FC = () => {
         };
 
         fetchData();
-        const interval = setInterval(fetchData, 5000); // Auto-refresh every 5 sec
+        const interval = setInterval(fetchData, 10000); // Auto-refresh every 10 sec
 
         return () => clearInterval(interval);
     },[]);
 
     if (!data) return <p>Loading data...</p>
 
+    const tempClass = data.temperature > 80 ? "bg-red-500/50 text-white font-bold" : "bg-sky-500/50";
+
     return (
         <div style={{ padding: "20px" }}>
-        <h1>Remote Equipment Monitoring</h1>
+        <h1 className="text-2xl font-bold">Remote Equipment Monitoring</h1>
         <StartStopButtons /> 
-        <p><strong>Machine ID:</strong> {data.machine_id}</p>
-        <p><strong>Temperature:</strong> {data.temperature} °C</p>
-        <p><strong>Pressure:</strong> {data.pressure} Pa</p>
-        <p><strong>Humidity:</strong> {data.humidity} %</p>
-        <p><strong>Timestamp:</strong> {data.timestamp}</p>
+        
+        {data.temperature > 80 && (<p className="text-red-700 font-bold text-lg mt-2">Warning! High Temperature Detected!</p>)}
+
+        <p><strong>Machine ID:</strong><a className="bg-sky-500/50"> {data.machine_id}</a></p>
+        <p><strong>Temperature:</strong><a className={tempClass}> {data.temperature} °C</a></p>
+        <p><strong>Pressure:</strong><a className="bg-sky-500/50"> {data.pressure} Pa</a></p>
+        <p><strong>Humidity:</strong><a className="bg-sky-500/50"> {data.humidity} %</a></p>
+        <p><strong>Timestamp:</strong><a className="bg-sky-500/50"> {data.timestamp}</a></p>
       </div>
     );
 };
--- components/StartStopButtons.tsx
+++ components/StartStopButtons.tsx
@@ -6,27 +6,6 @@ const API_URL = "https://wy9dediidb.execute-api.ap-southeast-2.amazonaws.com";
 const StartStopButtons = () => {
   // using state hook to control the button
   const [isGenerating, setIsGenerating] = useState(false);
-  const [countdown, setCountdown] = useState(60);
-
-  // Timer function
-  useEffect(() => {
-    let timer: NodeJS.Timeout;
-    if (isGenerating) {
-      // update countdown timer every second
-      timer = setInterval(() => {
-        setCountdown((prev) => {
-          if (prev === 1) {
-            return 60; // reset timer when it reaches to 0
-          }
-          return prev - 1; // decrease by 1
-        });
-      }, 1000);
-    } else {
-      setCountdown(60); // reset countdown when stopped
-    }
-
-    return () => clearInterval(timer);
-  }, [isGenerating]);
 
   const startSensorData = async () => {
     const response = await axios.post(`${API_URL}/start`);
@@ -44,9 +23,6 @@ const StartStopButtons = () => {
     <div className="flex space-x-4 mt-4">
       {isGenerating ? (
         <>
-          <p className="text-lg font-semibold text-blue-500">
-            Next update in: {countdown} seconds
-          </p>
           <button
             onClick={stopSensorData}
             className="px-4 py-2 text-white font-semibold rounded-lg shadow-md bg-red-500 hover:bg-red-600 transition"
